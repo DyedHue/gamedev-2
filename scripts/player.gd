273be755075a5317fbe5844332d595cf92567662
fuadhossainjehan <111167610+fuadhossainjehan@users.noreply.github.com>
2026-02-27 17:34:25 +0600

    Fuad

--- scripts/player.gd
+++ scripts/player.gd
@@ -15,34 +15,30 @@ class PlayerState:
 	var is_wall_sliding: bool = false
 
 	# Computed properties (Functions in GDScript)
-	func is_jumping() -> bool:
-		return vertical == VerticalState.GROUND_JUMP or vertical == VerticalState.AIR_JUMP
 	
 	func is_moving() -> bool:
 		return horizontal == HorizontalState.WALK or horizontal == HorizontalState.RUN
 
-class HasAbility:
-	var dash: bool = false
-	var wall_slide: bool = false
-	var faydown_cloak: bool = false
+
+@export var can_dash: bool = false
+@export var can_wall_slide: bool = false
+@export var can_variable_jump: bool = false
 
 # --- Main Script ---
 
 var state: PlayerState = PlayerState.new()
-var has_ability: HasAbility = HasAbility.new()
 
-@export var WALK_SPEED: float = 300.0
-@export var RUN_SPEED: float = 600.0
-@export var JUMP_VELOCITY: float = -550.0
+@export var WALK_SPEED: float = 400.0
+@export var RUN_SPEED: float = 700.0
+@export var JUMP_VELOCITY: float = -650.0
 @export var WALL_JUMP_SPEED: float = 700.0
 @export var GRAVITY = 2800
 
 const MAX_JUMP_DURATION: float = 0.25
-const MAX_AIR_JUMP_DURATION: float = 0.2
 const MAX_WALL_JUMP_DURATION: float = 0.15
 const MAX_WALL_JUMP_BACK_DUR: float = 0.15
 
-const MAX_AIR_JUMP_CHARGE: int = 1
+const MAX_AIR_JUMP_CHARGE: int = 0
 var air_jump_charge: int
 
 var jump_duration: float = 0.0
@@ -56,14 +52,15 @@ var frame_count: int = 0
 
 func _ready() -> void:
 	air_jump_charge = MAX_AIR_JUMP_CHARGE
-	has_ability.wall_slide = true
-	has_ability.dash = true
+	#can_wall_slide = true
+	can_dash = true
+	can_variable_jump = true
 
 func _physics_process(delta: float) -> void:
 	var gravity_vec: Vector2 = handle_gravity(delta)
 	var movement_vec: Vector2 = handle_movement()
 	var jump_vec: Vector2 = handle_jump(delta)
-	var run_jump_vec: Vector2 = handle_run_jump(delta)
+	#var run_jump_vec: Vector2 = handle_run_jump(delta)
 	var wall_jump_vec: Vector2 = handle_wall_jump()
 	
 	var current_velocity: Vector2 = Vector2.ZERO
@@ -72,10 +69,10 @@ func _physics_process(delta: float) -> void:
 	current_velocity.x += movement_vec.x
 	
 	if jump_vec != Vector2.ZERO:
-		current_velocity.y = jump_vec.y
+		current_velocity.y = jump_vec.y	
 		
-	if run_jump_vec != Vector2.ZERO:
-		current_velocity = run_jump_vec
+	# if run_jump_vec != Vector2.ZERO:
+	# 	current_velocity = run_jump_vec
 		
 	if wall_jump_vec != Vector2.ZERO:
 		current_velocity.x = wall_jump_vec.x
@@ -91,24 +88,7 @@ func _physics_process(delta: float) -> void:
 	show_debug()
 	frame_count += 1
 
-func show_debug() -> void:
-	var slide_str = "sliding" if state.is_wall_sliding else "notSlid"
-	
-	var new_debug_info = "%f, %f %s    ⌚: %f\n     🧗⌚: %f  H:%s  V:%s  🔋: %d\n" % [
-		velocity.x, 
-		velocity.y, 
-		slide_str, 
-		jump_duration,
-		wall_jump_duration, 
-		HorizontalState.keys()[state.horizontal], 
-		VerticalState.keys()[state.vertical], 
-		air_jump_charge
-	]
 
-	if new_debug_info != debug_info:
-		print("%d: %s" % [frame_count, new_debug_info])
-	
-	debug_info = new_debug_info
 
 func handle_gravity(delta: float) -> Vector2:
 	var gravity_vec = Vector2.ZERO
@@ -127,7 +107,7 @@ func handle_movement() -> Vector2:
 	var cur_speed = WALK_SPEED
 	var running = false
 	
-	if Input.is_action_pressed("dash") and has_ability.dash and is_on_floor():
+	if Input.is_action_pressed("dash") and can_dash and is_on_floor():
 		cur_speed = RUN_SPEED
 		running = true
 		
@@ -143,24 +123,17 @@ func handle_jump(delta: float) -> Vector2:
 	var jump_vec = Vector2.ZERO
 	
 	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
+		if is_on_floor() && !is_on_wall():
 			jump_vec.y = JUMP_VELOCITY
 			state.vertical = VerticalState.GROUND_JUMP
-		elif state.is_airborne:
-			if air_jump_charge != 0:
-				jump_vec.y = JUMP_VELOCITY
-				state.vertical = VerticalState.AIR_JUMP
-				air_jump_charge -= 1
 				
-	elif Input.is_action_pressed("jump"):
-		if state.is_jumping():
-			var limit = MAX_JUMP_DURATION if state.vertical == VerticalState.GROUND_JUMP else MAX_AIR_JUMP_DURATION
-			if jump_duration < limit:
+	elif Input.is_action_pressed("jump") and can_variable_jump:
+		if state.vertical == VerticalState.GROUND_JUMP:
+			if jump_duration < MAX_JUMP_DURATION:
 				jump_vec.y = JUMP_VELOCITY
 				jump_duration += delta
-				
-	elif Input.is_action_just_released("jump"):
-		jump_duration = 0.0
+	else:
+		state.vertical = VerticalState.NONE
 		
 	return jump_vec
 
@@ -197,8 +170,8 @@ func handle_wall_jump() -> Vector2:
 
 
 func pre_update_state(_delta: float, temp_velocity: Vector2) -> Vector2:
-	if is_on_wall() and not is_on_floor():
-		if not state.is_wall_sliding and has_ability.wall_slide:
+	if is_on_wall():
+		if not state.is_wall_sliding and can_wall_slide:
 			temp_velocity.y = 0
 			jump_duration = 0.0
 			air_jump_charge = MAX_AIR_JUMP_CHARGE
@@ -227,10 +200,31 @@ func post_update_state() -> void:
 		if velocity.x == 0:
 			state.horizontal = HorizontalState.NONE
 			
-	if not state.is_jumping():
+	if not state.vertical == VerticalState.GROUND_JUMP:
 		jump_duration = 0.0
 		
 	if not is_on_floor() and not is_on_wall():
 		state.is_airborne = true
 	else:
 		state.is_airborne = false
+
+func show_debug() -> void:
+	var slide_str = "sliding" if state.is_wall_sliding else "notSlid"
+	
+	var new_debug_info = "%f, %f %s    ⌚: %f\n     🧗⌚: %f  H:%s  V:%s  🔋: %d floor: %s wall: %s\n" % [
+		velocity.x, 
+		velocity.y, 
+		slide_str, 
+		jump_duration,
+		wall_jump_duration, 
+		HorizontalState.keys()[state.horizontal], 
+		VerticalState.keys()[state.vertical], 
+		air_jump_charge,
+		is_on_floor(),
+		is_on_wall()
+	]
+
+	if new_debug_info != debug_info:
+		print("%d: %s" % [frame_count, new_debug_info])
+	
+	debug_info = new_debug_info
