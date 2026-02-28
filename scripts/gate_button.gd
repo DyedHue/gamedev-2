9496f9f2761af529449ede6553698a47e2f36d66
Ad.Haider() <adiathaider@gmail.com>
2026-02-28 10:41:51 +0000

    gameover

--- scripts/gate_button.gd
+++ scripts/gate_button.gd
@@ -5,10 +5,17 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func on_area_entered(other_area):
 	hide()
+	$Sprite2D.hide()
+	$Sprite2D2.show()
 	gate_opened=true
+	$"../gateopen".show()
+	$"../gate".hide()
 	
 func _ready() -> void:
-	area_entered.connect(on_area_entered) # Replace with function body.
+	area_entered.connect(on_area_entered) 
+	$Sprite2D.show()
+	$Sprite2D2.hide()
+	$"../gateopen".hide()# Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
