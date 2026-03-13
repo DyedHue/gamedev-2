009a06b664feec2fcbbbaf19dd31f8fac5eef3f9
DyedHue <thashir2007@gmail.com>
2026-03-14 02:28:53 +0600

    pickaxe animation and gameover animation

--- scripts/hitbox.gd
+++ scripts/hitbox.gd
@@ -2,7 +2,6 @@ class_name myhitbox
 extends Area2D
 
 @onready var has_pickaxe
-# Use @onready to find the child once the node enters the scene tree
 @onready var col = $CollisionShape2D
 
 var timer: float = 0
@@ -10,23 +9,18 @@ var timer: float = 0
 func _init() -> void:
 	collision_layer = 2
 	collision_mask = 0
-	# Do NOT use $ here. Wait for _ready.
 
 func _ready() -> void:
 	# Start with the hitbox disabled
 	col.set_deferred("disabled", true)
 
 func _process(delta) -> void:
-	#pass
-	 #1. Start the attack
-	has_pickaxe=$"../../..".has_pickaxe
+	has_pickaxe=$"../..".has_pickaxe
 	if Input.is_action_just_pressed("attack"):
 		col.set_deferred("disabled", !has_pickaxe)
-		timer = 0.1 # Set a small window of time for the hit
+		timer = 0.1
 		
-	# 2. Count down the timer if an attack is active
 	if timer > 0:
 		timer -= delta
-		# 3. Disable when time runs out
 		if timer <= 0:
 			col.set_deferred("disabled", true)
