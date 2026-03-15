1e94d71054ea9ce81b14edfcd07a7764087584e1
DyedHue <thashir2007@gmail.com>
2026-03-16 04:06:07 +0600

    final submission

--- scripts/sound_player.gd
+++ scripts/sound_player.gd
@@ -1,5 +1,6 @@
 extends Node2D
 @onready var player: CharacterBody2D = $".."
+@onready var walk: AudioStreamPlayer = $"../../SFX/walk"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,6 +9,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-	#if player.velocity.x != 0 and player.is_on_floor():
-		#$"../../SFX/walk".play()
+	
+	if abs(player.velocity.x) > 600 and player.is_on_floor():
+		walk.pitch_scale = 1.4
+		if !walk.playing:
+			walk.play()
+	elif player.velocity.x != 0 and player.is_on_floor():
+		walk.pitch_scale = 1.18
+		if !walk.playing:
+			walk.play()
+	else:
+		if walk.playing:
+			walk.stop()
