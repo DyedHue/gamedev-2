df5de50b94d226ac865baafb7e99c8963f51cca8
DyedHue <thashir2007@gmail.com>
2026-03-15 21:03:43 +0600

    9:03 pm

---
+++ scripts/grass_background_1.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+@onready var player: CharacterBody2D = $"../../../Player"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player.position.x > 4571.0 && player.position.y < 3486.0:
+		visible = true
+	else:
+		visible = false
